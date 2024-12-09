c05b2b31b83f9b739bcf858692500c4ca307897f
Nataliia <nataliia.kulieshova@gmail.com>
2024-12-08 17:50:10 -0800

    Day 1: complete

--- day 1/day1.py
+++ day 1/day1.py
@@ -1,7 +1,6 @@
 data_file = open("data.txt", "r")
 list1 = []
 list2 = []
-distance = 0
 
 for line in data_file.readlines():
     entry = line.split()
@@ -11,7 +10,24 @@ for line in data_file.readlines():
 list1.sort()
 list2.sort()
 
+# PART 1
+distance = 0
 for i in range(len(list1)):
     distance += abs(list1[i] - list2[i])
 
 print(f"The total distance between two lists is {distance}")
+
+
+# PART 2
+similarity = 0
+left_set = set(list1)
+frequency_list2 = {}
+
+for number in list2:
+    frequency_list2[number] = frequency_list2.get(number, 0) + 1
+
+for number in left_set:
+    if number in frequency_list2:
+        similarity += number * frequency_list2[number]
+
+print(f"The similarity score for the lists is {similarity}")
